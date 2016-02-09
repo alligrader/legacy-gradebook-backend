df9fc82c67885d9596e63c2dbcb06a27204dcbc0
Robbie McKinstry <rem89@pitt.edu>
2016-02-09 01:51:58 -0500

    Adding autoenv functionality.

--- postinstall.bash
+++ postinstall.bash
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-export GO15VENDOREXPERIMENT=1
-export SHAMAN_ENV='DEVELOPMENT'
+function set_env {
+    export GO15VENDOREXPERIMENT=1
+    export SHAMAN_ENV='DEVELOPMENT'
+}
 
-sudo chown -R vagrant /opt/gopath
-go get github.com/Masterminds/glide
-glide up
+function install_deps {
+    sudo chown -R vagrant /opt/gopath
+    go get github.com/Masterminds/glide
+    glide up
+}
+
+function install_autoenv {
+    git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
+    echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
+    source ~/.bashrc
+    echo 'You might have to run source ./~bashrc before everything will work.'
+}
+
+function main {
+    set_env
+    install_deps
+    install_autoenv
+}
+
+main
