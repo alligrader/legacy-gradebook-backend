7d5440f45578462ed3dde9ae8cc391ca91de517d
Robbie McKinstry <rem89@pitt.edu>
2016-02-09 01:39:21 -0500

    Using config manager to set up envs

--- util/util.go
+++ util/util.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
 // This file holds the set up and tear down functions used to establish the database tables and columns.
 
 func ClearDatabase() {
@@ -8,4 +13,20 @@ func ClearDatabase() {
 
 func CreateTablesIfExists() {
 
+}
+
+func Configure() {
+	viper.SetEnvPrefix("SHAMAN")
+	viper.AutomaticEnv()
+}
+
+func ConfigureLogger() {
+	switch viper.Get("ENV") {
+	case "DEVELOPMENT":
+		log.SetFormatter(&log.TextFormatter{})
+	case "PRODUCTION":
+		log.SetFormatter(&log.JSONFormatter{})
+	default:
+		log.SetFormatter(&log.TextFormatter{})
+	}
 }
