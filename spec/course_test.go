a1695399585812b8578774fdff77646d9e23ac65
Robbie McKinstry <rem89@pitt.edu>
2016-02-26 01:05:04 -0500

    Adding tests for creating courses and teachers

--- spec/course_test.go
+++ spec/course_test.go
@@ -2,25 +2,52 @@ package spec
 
 import (
 	"testing"
+
+	. "github.com/gradeshaman/gradebook-backend/db"
+	. "github.com/gradeshaman/gradebook-backend/models"
+	"github.com/gradeshaman/gradebook-backend/util"
 )
 
-func TestCreateClass(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Testing dependent on Select")
+func TestPing() {
+	conn := util.ConnectToDB()
+	defer conn.Close()
+	if !conn.Ping() {
+		t.Error("Failed to ping the database")
 	}
 }
 
-func TestUpdateClass(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Testing dependent on Create")
+func TestCreateCourse(t *testing.T) {
+	conn := util.ConnectToDB()
+	defer conn.Close()
+	util.CreateEmptyDB(conn)
+	defer util.CleanDB(conn)
+
+	course := &Course{}
+	var courseStore db.CourseStore = &db.CourseMaker{db}
+
+	if !courseStore.Ping() {
+		t.Error("Did not connect to database.")
+	}
+
+	if err := courseStore.CreateCourse(course); err != nil {
+		t.Error(err)
+	}
+	if course.ID < 1 {
+		t.Error("Failed to generate a new ID for the course.")
+	}
+	if course.CreatedAt.IsZero() {
+		t.Error("Created At was not initialized.")
+	}
+	if course.LastUpdated.IsZero() {
+		t.Error("Last Updated was not initialized.")
 	}
 }
 
-func TestDeleteClass(t *testing.T) {
+func TestUpdateCourse(t *testing.T) {
 }
 
-func TestSelectClass(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Testing dependent on Create")
-	}
+func TestDeleteCourse(t *testing.T) {
+}
+
+func TestSelectCourse(t *testing.T) {
 }
--- spec/teacher_test.go
+++ spec/teacher_test.go
@@ -2,11 +2,36 @@ package spec
 
 import (
 	"testing"
+
+	. "github.com/gradeshaman/gradebook-backend/db"
+	. "github.com/gradeshaman/gradebook-backend/models"
+	"github.com/gradeshaman/gradebook-backend/util"
 )
 
 func TestCreateTeacher(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Testing dependent on Select")
+	conn := util.ConnectToDB()
+	defer conn.Close()
+	util.CreateEmptyDB(conn)
+	defer util.CleanDB(conn)
+
+	teacher := &teacher{CourseID: 5}
+	var teacherStore db.TeacherStore = &db.TeacherMaker{db}
+
+	if !teacherStore.Ping() {
+		t.Error("Did not connect to database.")
+	}
+
+	if err := teacherStore.CreateTeacher(teacher); err != nil {
+		t.Error(err)
+	}
+	if teacher.ID < 1 {
+		t.Error("Failed to generate a new ID for the course.")
+	}
+	if teacher.CreatedAt.IsZero() {
+		t.Error("Created At was not initialized.")
+	}
+	if teacher.LastUpdated.IsZero() {
+		t.Error("Last Updated was not initialized.")
 	}
 }
 
