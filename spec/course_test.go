df410c31eafd012388f038fb11e23b62c00a35a9
Robbie McKinstry <rem89@pitt.edu>
2016-02-29 00:50:08 -0500

    misc changes and fixes to spec, mostly obvious  changes

--- spec/course_test.go
+++ spec/course_test.go
@@ -3,44 +3,12 @@ package spec
 import (
 	"testing"
 
-	. "github.com/gradeshaman/gradebook-backend/db"
-	. "github.com/gradeshaman/gradebook-backend/models"
-	"github.com/gradeshaman/gradebook-backend/util"
+	_ "github.com/gradeshaman/gradebook-backend/db"
+	_ "github.com/gradeshaman/gradebook-backend/models"
+	_ "github.com/gradeshaman/gradebook-backend/util"
 )
 
-func TestPing() {
-	conn := util.ConnectToDB()
-	defer conn.Close()
-	if !conn.Ping() {
-		t.Error("Failed to ping the database")
-	}
-}
-
 func TestCreateCourse(t *testing.T) {
-	conn := util.ConnectToDB()
-	defer conn.Close()
-	util.CreateEmptyDB(conn)
-	defer util.CleanDB(conn)
-
-	course := &Course{}
-	var courseStore db.CourseStore = &db.CourseMaker{db}
-
-	if !courseStore.Ping() {
-		t.Error("Did not connect to database.")
-	}
-
-	if err := courseStore.CreateCourse(course); err != nil {
-		t.Error(err)
-	}
-	if course.ID < 1 {
-		t.Error("Failed to generate a new ID for the course.")
-	}
-	if course.CreatedAt.IsZero() {
-		t.Error("Created At was not initialized.")
-	}
-	if course.LastUpdated.IsZero() {
-		t.Error("Last Updated was not initialized.")
-	}
 }
 
 func TestUpdateCourse(t *testing.T) {
--- spec/teacher_test.go
+++ spec/teacher_test.go
@@ -3,36 +3,12 @@ package spec
 import (
 	"testing"
 
-	. "github.com/gradeshaman/gradebook-backend/db"
-	. "github.com/gradeshaman/gradebook-backend/models"
-	"github.com/gradeshaman/gradebook-backend/util"
+	_ "github.com/gradeshaman/gradebook-backend/db"
+	_ "github.com/gradeshaman/gradebook-backend/models"
+	_ "github.com/gradeshaman/gradebook-backend/util"
 )
 
 func TestCreateTeacher(t *testing.T) {
-	conn := util.ConnectToDB()
-	defer conn.Close()
-	util.CreateEmptyDB(conn)
-	defer util.CleanDB(conn)
-
-	teacher := &teacher{CourseID: 5}
-	var teacherStore db.TeacherStore = &db.TeacherMaker{db}
-
-	if !teacherStore.Ping() {
-		t.Error("Did not connect to database.")
-	}
-
-	if err := teacherStore.CreateTeacher(teacher); err != nil {
-		t.Error(err)
-	}
-	if teacher.ID < 1 {
-		t.Error("Failed to generate a new ID for the course.")
-	}
-	if teacher.CreatedAt.IsZero() {
-		t.Error("Created At was not initialized.")
-	}
-	if teacher.LastUpdated.IsZero() {
-		t.Error("Last Updated was not initialized.")
-	}
 }
 
 func TestUpdateTeacher(t *testing.T) {
