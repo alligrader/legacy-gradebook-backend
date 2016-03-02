05b22dd1aefdb56b9f7e8777d4d28afaeea12aa5
Robbie McKinstry <rem89@pitt.edu>
2016-03-02 12:37:47 -0500

    OAuth proof of concept!
    I'm in the dirty stage right now; I'll clean this up before I merge.

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,7 @@
 Vagrant.configure(2) do |config|
 
   config.vm.box = "ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 443, host: 443
   config.vm.network "forwarded_port", guest: 8000, host: 8000
   config.vm.network "forwarded_port", guest: 5000, host: 5000
   config.vm.network "forwarded_port", guest: 8080, host: 8080
--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 
 	. "github.com/gradeshaman/gradebook-backend/tasks"
 	"github.com/gradeshaman/gradebook-backend/util"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/spf13/viper"
 )
+
+func init() {
+
+	gothic.Store = sessions.NewFilesystemStore(os.TempDir(), []byte("goth-example"))
+	gothic.GetProviderName = func(r *http.Request) (string, error) {
+		vars := mux.Vars(r)
+		provider := vars["provider"]
+
+		if provider == "" {
+			provider = vars[":provider"]
+		}
+		if provider == "" {
+			return provider, errors.New("you must select a provider")
+		}
+		return provider, nil
+	}
+}
 
 func main() {
 
@@ -18,9 +43,14 @@ func main() {
 	util.Configure()
 	util.ConfigureLogger()
 
+	githubKey := viper.GetString("AUTH_CLIENT_ID")
+	githubSecret := viper.GetString("AUTH_CLIENT_SECRET")
+	goth.UseProviders(github.New(githubKey, githubSecret, ""))
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/auth/callback", AuthCallback)
+	r.HandleFunc("/auth/{provider}", auth) // gothic.BeginAuthHandler)
+	r.HandleFunc("/auth/{provider}/callback", AuthCallback)
 	http.Handle("/", r)
 	log.Println("Running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
@@ -30,6 +60,33 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, GradeShaman!")
 }
 
+func auth(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	provider := vars["provider"]
+	log.Warn(provider)
+	gothic.BeginAuthHandler(w, r)
+}
+
 func AuthCallback(w http.ResponseWriter, r *http.Request) {
 
+	fmt.Println("State: ", gothic.GetState(r))
+
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	t, _ := template.New("foo").Parse(userTemplate)
+	t.Execute(w, user)
 }
+
+var userTemplate = `
+<p>Name: {{.Name}}</p>p>
+<p>Email: {{.Email}}</p>p>
+<p>NickName: {{.NickName}}</p>p>
+<p>Location: {{.Location}}</p>p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></img>p>
+<p>Description: {{.Description}}</p>p>
+<p>UserID: {{.UserID}}</p>p>
+<p>AccessToken: {{.AccessToken}}</p>p>
+`
