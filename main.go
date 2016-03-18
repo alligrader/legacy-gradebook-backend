b89cc009569edf8e5322c0ebfbc87ef7fcae16f2
Natalie Boehm <nmb56@pitt.edu>
2016-03-17 23:15:09 -0400

    created new oauth package, moved user html templating to user.html

--- main.go
+++ main.go
@@ -1,64 +1,16 @@
 package main
 
 import (
-	"crypto/subtle"
-	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
-	"os"
-	"strconv"
-	"time"
 
+	"github.com/gradeshaman/gradebook-backend/oauth"
 	. "github.com/gradeshaman/gradebook-backend/tasks"
-	"github.com/gradeshaman/gradebook-backend/util"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/github"
-	"github.com/spf13/viper"
-	"golang.org/x/crypto/bcrypt"
 )
-
-var bits []byte
-
-func init() {
-	util.Configure()
-	util.ConfigureLogger()
-	githubKey := viper.GetString("AUTH_CLIENT_ID")
-	githubSecret := viper.GetString("AUTH_CLIENT_SECRET")
-	goth.UseProviders(github.New(githubKey, githubSecret, "", "user:email", "repo", "admin:repo_hook", "admin:org_hook", "admin:org"))
-
-	t := time.Now().UnixNano()
-	hash := strconv.Itoa(int(t))
-	var err error
-
-	bits, err = bcrypt.GenerateFromPassword([]byte(hash), bcrypt.DefaultCost)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	gothic.SetState = func(req *http.Request) string {
-		return string(bits)
-	}
-
-	gothic.Store = sessions.NewFilesystemStore(os.TempDir(), bits)
-	gothic.GetProviderName = func(r *http.Request) (string, error) {
-		vars := mux.Vars(r)
-		provider := vars["provider"]
-
-		if provider == "" {
-			provider = vars[":provider"]
-		}
-		if provider == "" {
-			return provider, errors.New("you must select a provider")
-		}
-		return provider, nil
-	}
-}
 
 func main() {
 
@@ -66,8 +18,10 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+
 	r.HandleFunc("/auth/{provider}", gothic.BeginAuthHandler)
-	r.HandleFunc("/auth/{provider}/callback", AuthCallback)
+	r.HandleFunc("/auth/{provider}/callback", oauth.AuthCallback)
+
 	http.Handle("/", r)
 	log.Println("Running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
@@ -76,35 +30,3 @@ func main() {
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, GradeShaman!")
 }
-
-func AuthCallback(w http.ResponseWriter, r *http.Request) {
-
-	observedState := []byte(gothic.GetState(r))
-	expectedState := bits
-
-	if subtle.ConstantTimeCompare(observedState, expectedState) != 1 {
-		http.Error(w, "State sent did not match state received.", 400)
-		log.Error(string(observedState))
-		log.Error(string(expectedState))
-		return
-	}
-
-	user, err := gothic.CompleteUserAuth(w, r)
-	if err != nil {
-		fmt.Fprintln(w, err)
-		return
-	}
-	t, _ := template.New("foo").Parse(userTemplate)
-	t.Execute(w, user)
-}
-
-var userTemplate = `
-<p>Name: {{.Name}}</p>
-<p>Email: {{.Email}}</p>
-<p>NickName: {{.NickName}}</p>
-<p>Location: {{.Location}}</p>
-<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></img></p>
-<p>Description: {{.Description}}</p>
-<p>UserID: {{.UserID}}</p>
-<p>AccessToken: {{.AccessToken}}</p>
-`
---
+++ oauth/oauth.go
@@ -0,0 +1,89 @@
+package oauth
+
+import (
+	"crypto/subtle"
+	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gradeshaman/gradebook-backend/util"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var bits []byte
+
+func init() {
+	util.Configure()
+	util.ConfigureLogger()
+
+	var (
+		githubKey         = viper.GetString("AUTH_CLIENT_ID")
+		githubSecret      = viper.GetString("AUTH_CLIENT_SECRET")
+		githubPermissions = []string{"user:email", "repo", "admin:repo_hook", "admin:org_hook", "admin:org"}
+		stateTime         = time.Now().UnixNano()
+		hash              = strconv.Itoa(int(stateTime))
+		err               error
+	)
+
+	goth.UseProviders(github.New(githubKey, githubSecret, "", githubPermissions[0], githubPermissions[1], githubPermissions[2], githubPermissions[3], githubPermissions[4]))
+
+	bits, err = bcrypt.GenerateFromPassword([]byte(hash), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gothic.SetState = func(req *http.Request) string {
+		return string(bits)
+	}
+
+	gothic.Store = sessions.NewFilesystemStore(os.TempDir(), bits)
+	gothic.GetProviderName = func(r *http.Request) (string, error) {
+		vars := mux.Vars(r)
+		provider := vars["provider"]
+
+		if provider == "" {
+			provider = vars[":provider"]
+		}
+		if provider == "" {
+			return provider, errors.New("you must select a provider")
+		}
+		return provider, nil
+	}
+}
+
+func AuthCallback(w http.ResponseWriter, r *http.Request) {
+
+	observedState := []byte(gothic.GetState(r))
+	expectedState := bits
+
+	if subtle.ConstantTimeCompare(observedState, expectedState) != 1 {
+		http.Error(w, "State sent did not match state received.", 400)
+		log.Error(string(observedState))
+		log.Error(string(expectedState))
+		return
+	}
+
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	t, err := template.ParseFiles("oauth/templates/user.html.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, user)
+}
