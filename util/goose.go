1f96e33916ef55f724765a5e1eae9058922c6821
Robbie McKinstry <rem89@pitt.edu>
2016-05-24 10:53:18 -0400

    refactored goose utils into their own file

---
+++ util/goose.go
@@ -0,0 +1,54 @@
+package util
+
+import (
+	"strings"
+
+	"bitbucket.org/liamstask/goose/lib/goose"
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func newGooseConf() *goose.DBConf {
+
+	var p string = viper.GetString("GOOSE_DIR")
+	var env string = strings.ToLower(viper.GetString("ENV"))
+	var schema string = "db"
+	g, err := goose.NewDBConf(p, env, schema)
+	if err != nil {
+		panic(err)
+	}
+	return g
+}
+
+func Up() {
+
+	var dirpath string = viper.GetString("GOOSE_DIR")
+	cfg := newGooseConf()
+	version, err := goose.GetMostRecentDBVersion(dirpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = goose.RunMigrations(cfg, dirpath, version)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Down() {
+	var dirpath string = viper.GetString("GOOSE_DIR")
+	cfg := newGooseConf()
+	version, err := goose.GetMostRecentDBVersion(dirpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	previous, err := goose.GetPreviousDBVersion(dirpath, version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = goose.RunMigrations(cfg, dirpath, previous)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- util/util.go
+++ util/util.go
@@ -3,11 +3,9 @@ package util
 import (
 	"database/sql"
 	"fmt"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
 
-	"bitbucket.org/liamstask/goose/lib/goose"
 	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 
@@ -146,51 +144,6 @@ func GetAndMarshal(query string, db Execer, destination interface{}, args ...int
 
 	return err
 
-}
-
-func newGooseConf() *goose.DBConf {
-
-	var p string = viper.GetString("GOOSE_DIR")
-	var env string = strings.ToLower(viper.GetString("ENV"))
-	var schema string = "db"
-	g, err := goose.NewDBConf(p, env, schema)
-	if err != nil {
-		panic(err)
-	}
-	return g
-}
-
-func Up() {
-
-	var dirpath string = viper.GetString("GOOSE_DIR")
-	cfg := newGooseConf()
-	version, err := goose.GetMostRecentDBVersion(dirpath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = goose.RunMigrations(cfg, dirpath, version)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func Down() {
-	var dirpath string = viper.GetString("GOOSE_DIR")
-	cfg := newGooseConf()
-	version, err := goose.GetMostRecentDBVersion(dirpath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	previous, err := goose.GetPreviousDBVersion(dirpath, version)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = goose.RunMigrations(cfg, dirpath, previous)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func WithCleanDB(f func()) {
