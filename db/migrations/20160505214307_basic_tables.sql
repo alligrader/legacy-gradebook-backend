65f9453477648979ef89768825b8e23928065de1
Robbie McKinstry <rem89@pitt.edu>
2016-05-28 02:27:26 -0400

    adding the new privilege structure

--- db/migrations/20160505214307_basic_tables.sql
+++ db/migrations/20160505214307_basic_tables.sql
@@ -2,14 +2,57 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 create table t_user (
-    id int auto_increment PRIMARY KEY,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    id           int auto_increment PRIMARY KEY,
+    created_at   timestamp DEFAULT CURRENT_TIMESTAMP,
     last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     first_name varchar(255) NOT NULL,
     last_name  varchar(255) NOT NULL,
     username   varchar(255) UNIQUE NOT NULL,
-    password   varchar(255) NOT NULL
+    password   varchar(255) NOT NULL,
+    status     int          NOT NULL DEFAULT 0
+);
+
+-- role contains the description and UID of each role in the database
+create table role (
+    id          int auto_increment  PRIMARY KEY,
+    name        varchar(255)        NOT NULL,
+    description text                NOT NULL
+);
+
+-- role_lines maps the user.id to the role.id
+create table role_lines (
+    user_id int NOT NULL,
+    role_id int NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES t_user(id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    PRIMARY KEY (user_id, role_id)
+);
+
+create table action (
+    id              int auto_increment PRIMARY KEY,
+    title           varchar(255)       NOT NULL,
+    apply_object    tinyint            NOT NULL
+);
+
+insert into action(title, apply_object) values
+   ('read',     1),
+   ('write',    1),
+   ('delete',   1),
+   ('join',     1),
+   ('activate', 1),
+   ('passwd',   1),
+   ('list_all', 0);
+
+-- maps the role id to an action that it can do in a certain status.
+-- says a person with role X can do Y in state Z
+create table t_privileges (
+    role_id     int     NOT NULL,
+    action_id   int     NOT NULL,
+    status      int     NOT NULL,
+
+    PRIMARY KEY (role_id, action_id, status)
 );
 
 create table student (
@@ -88,7 +131,11 @@ DROP TABLE submission;
 DROP TABLE project;
 DROP TABLE course_teachers;
 DROP TABLE course_members;
+DROP TABLE course;
 DROP TABLE teacher;
 DROP TABLE student;
-DROP TABLE course;
+DROP TABLE t_privileges;
+DROP TABLE action;
+DROP TABLE role_lines;
+DROP TABLE role;
 DROP TABLE t_user;
