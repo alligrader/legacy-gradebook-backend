ffc00f37ca3c10c8106764f4084b20e541f215f7
Robbie McKinstry <rem89@pitt.edu>
2016-06-03 07:24:18 -0400

    new shas and bumping to go 1.7beta1

--- .appdeps/install_go.bash
+++ .appdeps/install_go.bash
@@ -7,9 +7,11 @@ sudo apt-get update
 sudo apt-get install -yq \
     git \
     mysql-client \
+    curl \
     vim-nox
 
 if [ ! -d /usr/local/go ]; then
-    wget -q "https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz"
-    sudo tar -C /usr/local -xvf go1.6.linux-amd64.tar.gz
+    curl -O -J -L "https://storage.googleapis.com/golang/go1.7beta1.linux-amd64.tar.gz"
+    # wget -q "https://storage.googleapis.com/golang/go1.7beta1.linux-amd64.tar.gz"
+    sudo tar -C /usr/local -xvf go1.7beta1.linux-amd64.tar.gz
 fi
