ac163b4d229143a2a7ee1b2d9473c5fb75e51ee8
Natalie Boehm <nmb56@pitt.edu>
2016-06-27 16:49:30 -0400

    pushEvent code, fails dredd

--- routes/hooks/webhooks.go
+++ routes/hooks/webhooks.go
@@ -1,13 +1,34 @@
 package hooks
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/alligrader/gradebook-backend/tasks"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/google/go-github/github"
 )
 
 func HandlePushEvent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	fmt.Fprintf(w, `{ "message": "Push Event Detected" }`+"\n")
+
+	var requestBody github.Event = github.Event{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		log.Error(err)
+	}
+
+	var message github.PushEvent
+
+	message = requestBody.Payload().(github.PushEvent)
+
+	tasks.Bus.PushCheckstyle(*message.PushID)
+	tasks.Bus.PushFindbugs(*message.PushID)
 }
 
 func HandleDeploymentEvent(w http.ResponseWriter, r *http.Request) {
